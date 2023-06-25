683171fc271cbddd5850bf0d10cfa828504e5da4
sitiaisyah14 <sitiaisyah4110@gmail.com>
2023-06-25 12:41:14 +0700

    update nilai preferensi

--- nilai_preferensi.php
+++ nilai_preferensi.php
@@ -2,27 +2,9 @@
 @session_start();
 include("konfig/koneksi.php");
 
-/*
- echo "<i>cek sessionn dplus</i>";    
-echo "<pre>";    
-print_r($_SESSION['dplus']);    
-echo "</pre>";  
-
-
-echo "<i>cek sessionn dmin</i>";    
-echo "<pre>";    
-print_r($_SESSION['dmin']);    
-echo "</pre>";  
-*/
-
-//print_r($_SESSION);
-
-
 if (!isset($_SESSION['ymax'])) {
 	include('jarak_solusi.php');
 }
-
-
 ?>
 
 <div class="box-header">
@@ -30,126 +12,118 @@ if (!isset($_SESSION['ymax'])) {
 </div>
 
 <font size="2px">
-<table class="table table-bordered table-responsive">
-	<thead>
-		<tr>
-			<th>
-				<center>Ranking</center>
-			</th>
-			<th>
-				<center>Nama</center>
-			</th>
-			<th>
-				<center>V<sub>i</sub></center>
-			</th>
-		</tr>
+	<table class="table table-bordered table-responsive">
+		<thead>
+			<tr>
+				<th>
+					<center>Ranking</center>
+				</th>
+				<th>
+					<center>Nama</center>
+				</th>
+				<th>
+					<center>V<sub>i</sub></center>
+				</th>
+			</tr>
 
-	</thead>
-	<tbody>
-		<?php
-		$i = 1;
-		$a = mysqli_query($k21, "select * from alternatif order by id_alternatif asc;");
-		echo "<tr>";
-		$sortir = array();
-		while ($da = mysqli_fetch_assoc($a)) {
+		</thead>
+		<tbody>
+			<?php
+			$i = 1;
+			$a = mysqli_query($k21, "select * from alternatif order by id_alternatif asc;");
+			echo "<tr>";
+			$sortir = array();
+			while ($da = mysqli_fetch_assoc($a)) {
+				$idalt = $da['id_alternatif'];
 
+				//ambil nilai
 
+				$n = mysqli_query($k21, "select * from nilai_matrik where id_alternatif='$idalt' order by id_matrik ASC");
 
-			$idalt = $da['id_alternatif'];
+				$c = 0;
+				$ymax = array();
+				while ($dn = mysqli_fetch_assoc($n)) {
+					$idk = $dn['id_kriteria'];
 
-			//ambil nilai
+					//nilai kuadrat
+					$nilai_kuadrat = 0;
+					$k = mysqli_query($k21, "select * from nilai_matrik where id_kriteria='$idk' order by id_matrik ASC ");
+					while ($dkuadrat = mysqli_fetch_assoc($k)) {
+						$nilai_kuadrat = $nilai_kuadrat + ($dkuadrat['nilai'] * $dkuadrat['nilai']);
+					}
 
-			$n = mysqli_query($k21, "select * from nilai_matrik where id_alternatif='$idalt' order by id_matrik ASC");
+					//hitung jml alternatif
+					$jml_alternatif = mysqli_query($k21, "select * from alternatif order by id_alternatif asc;");
+					$jml_a = mysqli_num_rows($jml_alternatif);
+					//nilai bobot kriteria (rata")
+					$bobot = 0;
+					$tnilai = 0;
 
-			$c = 0;
-			$ymax = array();
-			while ($dn = mysqli_fetch_assoc($n)) {
-				$idk = $dn['id_kriteria'];
+					$k2 = mysqli_query($k21, "select * from nilai_matrik where id_kriteria='$idk' order by id_matrik ASC ");
+					while ($dbobot = mysqli_fetch_assoc($k2)) {
+						$tnilai = $tnilai + $dbobot['nilai'];
+					}
+					$bobot = $tnilai / $jml_a;
 
+					//nilai bobot input
+					$b2 = mysqli_query($k21, "select * from kriteria where id_kriteria='$idk'");
+					$nbot = mysqli_fetch_assoc($b2);
+					$bot = $nbot['bobot'];
 
-				//nilai kuadrat
+					$v = round(($dn['nilai'] / sqrt($nilai_kuadrat)) * $bot);
 
-				$nilai_kuadrat = 0;
-				$k = mysqli_query($k21, "select * from nilai_matrik where id_kriteria='$idk' order by id_matrik ASC ");
-				while ($dkuadrat = mysqli_fetch_assoc($k)) {
-					$nilai_kuadrat = $nilai_kuadrat + ($dkuadrat['nilai'] * $dkuadrat['nilai']);
+					$ymax[$c] = $v;
+					$c;
+					$mak = max($ymax);
+					$min = min($ymax);
 				}
 
-				//hitung jml alternatif
-				$jml_alternatif = mysqli_query($k21, "select * from alternatif order by id_alternatif asc;");
-				$jml_a = mysqli_num_rows($jml_alternatif);
-				//nilai bobot kriteria (rata")
-				$bobot = 0;
-				$tnilai = 0;
-
-				$k2 = mysqli_query($k21, "select * from nilai_matrik where id_kriteria='$idk' order by id_matrik ASC ");
-				while ($dbobot = mysqli_fetch_assoc($k2)) {
-					$tnilai = $tnilai + $dbobot['nilai'];
-				}
-				$bobot = $tnilai / $jml_a;
-
-				//nilai bobot input
-				$b2 = mysqli_query($k21, "select * from kriteria where id_kriteria='$idk'");
-				$nbot = mysqli_fetch_assoc($b2);
-				$bot = $nbot['bobot'];
-
-				$v = round(($dn['nilai'] / sqrt($nilai_kuadrat)) * $bot);
-
-				$ymax[$c] = $v;
-				$c;
-				$mak = max($ymax);
-				$min = min($ymax);
+				$i++;
 			}
 
-			$i++;
-		}
+			foreach (@$_SESSION['dplus'] as $key => $dxmin) {
+				#ubah ke nol hasil akhir
+				$nilaid = 0;
+				$nilaiPre = 0;
+				$nilai = 0;
 
-		foreach (@$_SESSION['dplus'] as $key => $dxmin) {
-			#ubah ke nol hasil akhir
-			$nilaid = 0;
-			$nilaiPre = 0;
-			$nilai = 0;
+				$jarakm = $_SESSION['dmin'][$key];
+				$id_alt = $_SESSION['id_alt'][$key];
 
-			$jarakm = $_SESSION['dmin'][$key];
-			$id_alt = $_SESSION['id_alt'][$key];
+				//nama alternatif
+				$nama = mysqli_query($k21, "select * from alternatif where id_alternatif='$id_alt'");
+				$nm = mysqli_fetch_assoc($nama);
 
-			//nama alternatif
-			$nama = mysqli_query($k21, "select * from alternatif where id_alternatif='$id_alt'");
-			$nm = mysqli_fetch_assoc($nama);
+				$nilaiPre = $dxmin + $jarakm;
 
-			$nilaiPre = $dxmin + $jarakm;
-
-			$nilaid = $jarakm / $nilaiPre;
+				$nilaid = $jarakm / $nilaiPre;
 
 
-			$nilai = round($nilaid, 4);
+				$nilai = round($nilaid, 4);
 
-			//simpan ke tabel nilai preferensi
-			$nm = $nm['nm_alternatif'];
+				//simpan ke tabel nilai preferensi
+				$nm = $nm['nm_alternatif'];
 
-			$sql2 = mysqli_query($k21, "insert into nilai_preferensi (nm_alternatif,nilai) values('$nm','$nilai')");
+				$sql2 = mysqli_query($k21, "insert into nilai_preferensi (nm_alternatif,nilai) values('$nm','$nilai')");
+			}
 
-		}
-
-		//ambil data sesuai dengan nilai tertinggi
-		$i = 1;
-		$sql3 = mysqli_query($k21, "select * from nilai_preferensi  order by nilai desc");
-		while ($data3 = mysqli_fetch_assoc($sql3)) {
-			echo "<tr>
+			//ambil data sesuai dengan nilai tertinggi
+			$i = 1;
+			$sql3 = mysqli_query($k21, "select * from nilai_preferensi  order by nilai desc");
+			while ($data3 = mysqli_fetch_assoc($sql3)) {
+				echo "<tr>
 		<td>" . $i . "</td>
 		<td>$data3[nm_alternatif]</td>
 		<td>$data3[nilai]</td>
 		</tr>";
+				$i++;
+			}
 
-			$i++;
-		}
+			//kosongkan tabel nilai preferensi
+			$del = mysqli_query($k21, "delete from nilai_preferensi");
+			echo "</tr>";
+			?>
 
-		//kosongkan tabel nilai preferensi
-		$del = mysqli_query($k21, "delete from nilai_preferensi");
-
-		echo "</tr>";
-		?>
-
-	</tbody>
-</table>
+		</tbody>
+	</table>
 </font>
