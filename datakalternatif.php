90c8c1384fcfa9f9a37cb54618bfeab23dde9d8e
sitiaisyah14 <sitiaisyah4110@gmail.com>
2023-06-25 12:39:59 +0700

    update data alt dan criteria

--- datakalternatif.php
+++ datakalternatif.php
@@ -1,39 +1,51 @@
 <div class="table-responsive">
     <div class="panel panel-default">
         <font size="2px">
-        <!-- Default panel contents -->
-        <div class="panel-heading">DATA ALTERNATIF</div>
+            <!-- Default panel contents -->
+            <div class="panel-heading">DATA ALTERNATIF</div>
 
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th><center>Id Alternatif</center></th>
-                        <th><center>Nama Alternatif</center></th>
-                        <th><center>Pilihan</center></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    include("konfig/koneksi.php");
-
-                    $s = mysqli_query($k21, "select * from alternatif order by id_alternatif ASC");
-                    while ($d = mysqli_fetch_assoc($s)) {
-                    ?>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead>
                         <tr>
-                            <td><center><?php echo $d['id_alternatif']; ?></center></td>
-                            <td><center><?php echo $d['nm_alternatif']; ?></center></td>
-                            <td><center>
-                                <a href="?a=alternatif&k=ubaha&id=<?php echo $d['id_alternatif']; ?>" class="btn btn-warning">Ubah</a>
-                                <a href="hapusalternatif.php?id=<?php echo $d['id_alternatif']; ?>" class="btn btn-danger">Hapus</a>
-                                </center></td>
+                            <th>
+                                <center>Id Alternatif</center>
+                            </th>
+                            <th>
+                                <center>Nama Alternatif</center>
+                            </th>
+                            <th>
+                                <center>Pilihan</center>
+                            </th>
                         </tr>
-                    <?php
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        <?php
+                        include("konfig/koneksi.php");
+
+                        $s = mysqli_query($k21, "select * from alternatif order by id_alternatif ASC");
+                        while ($d = mysqli_fetch_assoc($s)) {
+                        ?>
+                            <tr>
+                                <td>
+                                    <center><?php echo $d['id_alternatif']; ?></center>
+                                </td>
+                                <td>
+                                    <center><?php echo $d['nm_alternatif']; ?></center>
+                                </td>
+                                <td>
+                                    <center>
+                                        <a href="?a=alternatif&k=ubaha&id=<?php echo $d['id_alternatif']; ?>" class="btn btn-warning">Ubah</a>
+                                        <a href="hapusalternatif.php?id=<?php echo $d['id_alternatif']; ?>" class="btn btn-danger">Hapus</a>
+                                    </center>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </font>
     </div>
 </div>
--- datakriteria.php
+++ datakriteria.php
@@ -5,82 +5,82 @@
     <div class="panel panel-default">
         <!-- Default panel contents -->
         <font size="2px">
-        <div class="panel-heading">DATA KRITERIA</div>
-        
-        <table class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>
-                        <center>Id Kriteria</center>
-                    </th>
-                    <th>
-                        <center>Nama Kriteria</center>
-                    </th>
-                    <th>
-                        <center>Bobot</center>
-                    </th>
-                    <th>
-                        <center>Poin 1</center>
-                    </th>
-                    <th>
-                        <center>Poin 2</center>
-                    </th>
-                    <th>
-                        <center>Poin 3</center>
-                    </th>
-                    <th>
-                        <center>Poin 4</center>
-                    </th>
-                    <th>
-                        <center>Sifat Kriteria</center>
-                    </th>
-                    <th>
-                        <center>Pilihan</center>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                include("konfig/koneksi.php");
+            <div class="panel-heading">DATA KRITERIA</div>
 
-                $s = mysqli_query($k21, "select * from kriteria order by id_kriteria ASC");
-                while ($d = mysqli_fetch_assoc($s)) {
-                ?>
+            <table class="table table-bordered table-striped">
+                <thead>
                     <tr>
-                        <td>
-                            <center><?php echo $d['id_kriteria']; ?></center>
-                        </td>
-                        <td><?php echo $d['nama_kriteria']; ?></td>
-                        <td>
-                            <center><?php echo $d['bobot']; ?></center>
-                        </td>
-                        <td>
-                            <center><?php echo $d['poin1']; ?></center>
-                        </td>
-                        <td>
-                            <center><?php echo $d['poin2']; ?></center>
-                        </td>
-                        <td>
-                            <center><?php echo $d['poin3']; ?></center>
-                        </td>
-                        <td>
-                            <center><?php echo $d['poin4']; ?></center>
-                        </td>
-                        
-                        <td>
-                            <center><?php echo $d['sifat']; ?></center>
-                        </td>
-                        <td>
-                            <a href="?a=kriteria&k=ubahk&id=<?php echo $d['id_kriteria']; ?>" class="btn btn-warning">Ubah</a>
-                            <a href="hapus.php?id=<?php echo $d['id_kriteria']; ?>" class="btn btn-danger">Hapus</a>
-                        </td>
+                        <th>
+                            <center>Id Kriteria</center>
+                        </th>
+                        <th>
+                            <center>Nama Kriteria</center>
+                        </th>
+                        <th>
+                            <center>Bobot</center>
+                        </th>
+                        <th>
+                            <center>Poin 1</center>
+                        </th>
+                        <th>
+                            <center>Poin 2</center>
+                        </th>
+                        <th>
+                            <center>Poin 3</center>
+                        </th>
+                        <th>
+                            <center>Poin 4</center>
+                        </th>
+                        <th>
+                            <center>Sifat Kriteria</center>
+                        </th>
+                        <th>
+                            <center>Pilihan</center>
+                        </th>
                     </tr>
-                <?php
-                }
-                ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php
+                    include("konfig/koneksi.php");
+
+                    $s = mysqli_query($k21, "select * from kriteria order by id_kriteria ASC");
+                    while ($d = mysqli_fetch_assoc($s)) {
+                    ?>
+                        <tr>
+                            <td>
+                                <center><?php echo $d['id_kriteria']; ?></center>
+                            </td>
+                            <td><?php echo $d['nama_kriteria']; ?></td>
+                            <td>
+                                <center><?php echo $d['bobot']; ?></center>
+                            </td>
+                            <td>
+                                <center><?php echo $d['poin1']; ?></center>
+                            </td>
+                            <td>
+                                <center><?php echo $d['poin2']; ?></center>
+                            </td>
+                            <td>
+                                <center><?php echo $d['poin3']; ?></center>
+                            </td>
+                            <td>
+                                <center><?php echo $d['poin4']; ?></center>
+                            </td>
+
+                            <td>
+                                <center><?php echo $d['sifat']; ?></center>
+                            </td>
+                            <td>
+                                <a href="?a=kriteria&k=ubahk&id=<?php echo $d['id_kriteria']; ?>" class="btn btn-warning">Ubah</a>
+                                <a href="hapus.php?id=<?php echo $d['id_kriteria']; ?>" class="btn btn-danger">Hapus</a>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </font>
     </div>
-    </div>
+</div>
 </div>
