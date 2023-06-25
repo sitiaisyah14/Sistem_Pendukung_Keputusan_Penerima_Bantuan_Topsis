d04769b1bd5e0051213080a01f1b247144434664
sitiaisyah14 <sitiaisyah4110@gmail.com>
2023-06-25 12:41:44 +0700

    update jarak solusi

--- nilaimatrik.php
+++ nilaimatrik.php
@@ -1,87 +1,76 @@
 <h1>Nilai Matriks</h1>
 <font size="2px">
-<ul class="nav nav-tabs">
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="index.php?a=kriteria&k=kriteria">Isi Nilai Matriks</a></li>
+	</ul>
+	<div class="box-header">
+		<h3 class="box-title">Tambah Nilai Matriks</h3>
+	</div>
 
-	<li class="active"><a href="index.php?a=kriteria&k=kriteria">Isi Nilai Matriks</a></li>
+	<form method="POST" action="">
+		<div class="form-group">
+			<label class="control-label col-lg-2">Id Alternatif</label>
+			<div class="col-lg-4">
+				<select name="id_alt" class="form-control">
+					<?php
+					include("konfig/koneksi.php");
+					$s = mysqli_query($k21, "select * from alternatif");
+					while ($d = mysqli_fetch_assoc($s)) {
+					?>
 
-
-
-</ul>
-<div class="box-header">
-	<h3 class="box-title">Tambah Nilai Matriks</h3>
-</div>
-
-
-<form method="POST" action="">
-	<div class="form-group">
-		<label class="control-label col-lg-2">Id Alternatif</label>
-		<div class="col-lg-4">
-			<select name="id_alt" class="form-control">
-				<?php
-				include("konfig/koneksi.php");
-				$s = mysqli_query($k21, "select * from alternatif");
-				while ($d = mysqli_fetch_assoc($s)) {
-				?>
-
-					<option value="<?php echo $d['id_alternatif'] ?>"><?php echo $d['id_alternatif'] . " | " . $d['nm_alternatif'] ?></option>
-				<?php
-				}
-				?>
-			</select>
-
-
+						<option value="<?php echo $d['id_alternatif'] ?>"><?php echo $d['id_alternatif'] . " | " . $d['nm_alternatif'] ?></option>
+					<?php
+					}
+					?>
+				</select>
+			</div>
 		</div>
+		<br />
+		<hr />
 
-	</div>
-	<br />
-	<hr />
-
-	<div class="form-group">
-		<?php
-		$i = 1;
-		$alt = mysqli_query($k21, "select * from kriteria");
-		//hitung jml kriteria		
-		$jml_krit = mysqli_num_rows($alt);
-
-		while ($dalt = mysqli_fetch_assoc($alt)) {
-		?>
-
-			<table align="left">
-				<tr>
-					<td width="200">
-						<label><?php echo $dalt['nama_kriteria']; ?></label>
-						<input type="hidden" name="id_krite<?php echo $i; ?>" value="<?php echo $dalt['id_kriteria']; ?>" />
-					</td>
-					<div class="col-md-8">
-						<td width="80">
-							<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin1']; ?>" /><?php echo $dalt['poin1']; ?>
-						</td>
-						<td width="80">
-							<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin2']; ?>" /><?php echo $dalt['poin2']; ?>
-						</td>
-						<td width="80">
-							<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin3']; ?>" /><?php echo $dalt['poin3']; ?>
-						</td>
-						<td width="80">
-							<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin4']; ?>" /><?php echo $dalt['poin4']; ?>
-						</td>
-				</tr>
-
+		<div class="form-group">
 			<?php
-			$i++;
-		}
+			$i = 1;
+			$alt = mysqli_query($k21, "select * from kriteria");
+			//hitung jml kriteria		
+			$jml_krit = mysqli_num_rows($alt);
+
+			while ($dalt = mysqli_fetch_assoc($alt)) {
 			?>
-			<tr>
-				<td colspan=5 align="center">
-					<input type="submit" name="simpan" value="Simpan" class="btn btn-primary">
-				</td>
-			</tr>
-			</table>
 
-	</div>
+				<table align="left">
+					<tr>
+						<td width="200">
+							<label><?php echo $dalt['nama_kriteria']; ?></label>
+							<input type="hidden" name="id_krite<?php echo $i; ?>" value="<?php echo $dalt['id_kriteria']; ?>" />
+						</td>
+						<div class="col-md-8">
+							<td width="80">
+								<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin1']; ?>" /><?php echo $dalt['poin1']; ?>
+							</td>
+							<td width="80">
+								<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin2']; ?>" /><?php echo $dalt['poin2']; ?>
+							</td>
+							<td width="80">
+								<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin3']; ?>" /><?php echo $dalt['poin3']; ?>
+							</td>
+							<td width="80">
+								<input type="radio" name="nilai<?php echo $i; ?>" value="<?php echo $dalt['poin4']; ?>" /><?php echo $dalt['poin4']; ?>
+							</td>
+					</tr>
 
-	</div>
-
+				<?php
+				$i++;
+			}
+				?>
+				<tr>
+					<td colspan=5 align="center">
+						<input type="submit" name="simpan" value="Simpan" class="btn btn-primary">
+					</td>
+				</tr>
+				</table>
+		</div>
+		</div>
 </font>
 </form>
 <?php
